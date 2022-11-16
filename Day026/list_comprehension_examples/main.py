3af8b131240d460396934a5fac0177c5d69a0cd1
wintermute111 <marshallbishop@gmail.com>
2022-11-15 22:18:30 -0500

    Initial commit

---
+++ Day026/list_comprehension_examples/main.py
@@ -0,0 +1,16 @@
+# new_list = [new_item for item in list if test]
+
+# nums = [1, 2, 3]
+# nl = [n+1 for n in nums]
+# print(nl)
+
+# name = "Mike"
+# new_list = [letter.lower() for letter in name]
+# print(new_list)
+
+# new_list = [num * 2 for num in range(1,5)]
+# print(new_list)
+
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# upper_long_names = [name.upper() for name in names if len(name) > 5]
+# print(upper_long_names)
